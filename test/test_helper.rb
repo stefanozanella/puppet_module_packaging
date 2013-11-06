0e1f0ea387c9a88f276d64dbb8af21ed7628eafb
Stefano Zanella <zanella.stefano@gmail.com>
2013-11-06 21:42:21 +0100

    Document helper functions.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,32 +8,40 @@ require 'tmpdir'
 
 require 'puppet_module/pkg/tasks'
 
+# Directory that contains test fixtures
 def fixture_dir
   File.join File.dirname(__FILE__), 'fixture'
 end
 
+# Path of the test module named `name`
 def fixture_module(name)
   File.join(fixture_dir, name)
 end
 
+# Creates an array of all increasing paths from dirs.first to dirs.last
+#
+# Ex: walk_path(['a','b','c']) # => ['a', 'a/b', 'a/b/c']
 def walk_path(dirs)
   (1..dirs.to_a.length).map do |k|
     File.join(*dirs.to_a.take(k))
   end
 end
 
+# Exec proc inside specified directory
 def do_into_dir(dir, t)
   Dir.chdir dir do
     t.call
   end
 end
 
+# Exec proc inside a temp dir
 def do_into_tmpdir(t)
   Dir.mktmpdir do |dir|
     do_into_dir(dir, t)
   end
 end
 
+# Exec proc inside a temp copy of fixture module `mod_name`
 def do_into_tmp_module(mod_name, t)
   do_into_tmpdir(proc {
     FileUtils.cp_r fixture_module(mod_name), mod_name
