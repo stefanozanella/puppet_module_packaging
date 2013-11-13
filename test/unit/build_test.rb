12073258f911b6ca1fb863f0df3ee5af57938388
Stefano Zanella <zanella.stefano@gmail.com>
2013-11-13 13:35:17 +0100

    Unify unit test for packaging tasks.
    Since RPM and Deb share common logic, it's totally useless to replicate it
    twice.

--- test/unit/build_test.rb
+++ test/unit/build_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
-describe PuppetModule::Pkg::Tasks::Deb do
+describe 'packaging tasks' do
   let(:deb_task)    { PuppetModule::Pkg::Tasks::Deb }
+  let(:rpm_task)    { PuppetModule::Pkg::Tasks::RPM }
   let(:sys)         { stub_everything }
   let(:mod) { OpenStruct.new(
     :name    => 'my_mod',
@@ -19,18 +20,37 @@ describe PuppetModule::Pkg::Tasks::Deb do
     deb_task.new(sys).invoke(mod, opts)
   end
 
-  it 'builds a deb package using mandatory module info' do
+  it 'builds a package using mandatory module info' do
     sys.expects(:sh).with(all_of(
       regexp_matches(/^fpm/),
       regexp_matches(/-s dir/),
-      regexp_matches(/-t deb/),
       regexp_matches(/-a all/),
       regexp_matches(/-n puppet-mod-#{mod.author}-#{mod.name}/),
-      regexp_matches(/-p #{opts.pkg_dir}\/puppet-mod-#{mod.author}-#{mod.name}_VERSION_ARCH.deb/),
       regexp_matches(/-v #{mod.version}/),
       regexp_matches(/-C #{opts.install_dir}/),
       regexp_matches(/\.$/)))
 
     deb_task.new(sys).invoke(mod, opts)
   end
+
+  describe 'deb task' do
+    it 'builds a deb package, using Debian conventions' do
+      sys.expects(:sh).with(all_of(
+        regexp_matches(/-t deb/),
+        regexp_matches(/-p #{opts.pkg_dir}\/puppet-mod-#{mod.author}-#{mod.name}_VERSION_ARCH.deb/)))
+  
+      deb_task.new(sys).invoke(mod, opts)
+    end
+  end
+
+  describe 'rpm task' do
+    it 'builds a rpm package, using RPM conventions' do
+      sys.expects(:sh).with(all_of(
+        regexp_matches(/-t rpm/),
+        regexp_matches(/-p #{opts.pkg_dir}\/puppet-mod-#{mod.author}-#{mod.name}-VERSION.ARCH.rpm/)))
+  
+      rpm_task.new(sys).invoke(mod, opts)
+    end
+  end
 end
+
